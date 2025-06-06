8c74cc826b65057e5b2d9d4f293352fa44e53171
bamcmanus <brent.mcmanus@me.com>
2025-06-06 16:26:47 -0700

    implement function that reads and outputs channel

--- main.go
+++ main.go
@@ -15,25 +15,38 @@ func main() {
 	}
 	defer file.Close()
 
-	var currentLine string
-	for {
-		buffer := make([]byte, 8)
-		var bytesRead int
-		if bytesRead, err = file.Read(buffer); err == io.EOF {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)	
-			}
-			break
-		} else if err != nil {
-			log.Fatal("error reading bytes")
-		}
-		byteString := string(buffer[:bytesRead])
-		parts := strings.Split(byteString, "\n")
-		for i:=range len(parts)-1 {
-			currentLine += parts[i]
-			fmt.Printf("read: %s\n", currentLine)	
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	strChan := make(chan string)
+	var currentLine string
+	go func() {
+		defer f.Close()
+		defer close(strChan)
+		for {
+			buffer := make([]byte, 8)
+			var bytesRead int
+			var err error
+			if bytesRead, err = f.Read(buffer); err == io.EOF {
+				if currentLine != "" {
+					strChan <- currentLine
+				}
+				break
+			} else if err != nil {
+				log.Fatal("error reading bytes")
+			}
+			byteString := string(buffer[:bytesRead])
+			parts := strings.Split(byteString, "\n")
+			for i:=range len(parts)-1 {
+				currentLine += parts[i]
+				strChan <- currentLine
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+	}()
+	return strChan
 }
