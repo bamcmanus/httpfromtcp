5b25e929f9be222d968056e099d25409225a0a59
bamcmanus <brent.mcmanus@me.com>
2025-06-06 18:03:49 -0700

    add udp sender and move code to listener pkg

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main () {
+	addr, err := net.ResolveUDPAddr("udp", ":42069")
+	if err != nil {
+		log.Fatal("failed to resolve UDP address")
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal("could not establish connection")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("error reading from stdin:", err)
+			continue
+		}
+		_, err = conn.Write([]byte(str))
+		if err != nil {
+			log.Println("error while writing bytes:", err)
+		}
+	}
+}
